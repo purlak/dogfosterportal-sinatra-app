30c536ccfd347f6382d989a07c16e5bb16aebf55
Purnima Lakshmi <codequeen1110@gmail.com>
2018-04-30 01:15:52 -0400

    added signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  get '/signup' do 
+    if session[:user_id]
+      redirect 'tweets/tweets'
+    else 
+      erb :index  
+    end  
+  end 
+
   
 
 end 
