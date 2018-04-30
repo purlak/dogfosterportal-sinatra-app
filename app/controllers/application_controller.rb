21ff0ec6874d8b99fc9c12ff5ad4e972fcd052e0
Purnima Lakshmi <codequeen1110@gmail.com>
2018-04-30 01:15:18 -0400

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     enable :sessions
     set :session_secret, "my_application_secret"
-    set :views, Proc.new { File.join(root, "../views/") }    
+    set :views, 'app/views'    
   end
 
   get / do
