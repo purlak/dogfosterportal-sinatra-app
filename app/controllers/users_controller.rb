a3410935fd5bcf475138f06369b22b7f1fb5147c
Purnima Lakshmi <codequeen1110@gmail.com>
2018-04-30 01:45:25 -0400

    added signup - post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
 
+  # Signup 
+
   get '/signup' do 
     if session[:user_id]
       redirect 'users/show_user_summary'
     else 
       erb :signup  
     end  
+  end 
+
+  post '/signup' do
+    @username = params[:username] 
+    @password = params[:password]
+    @email = params[:email]
+    
+    if @username == "" || @password == "" || @email == "" 
+      # to add flash message 
+      redirect '/signup'
+    else  
+      # to add flash message 
+      session[:user_id] = 1
+      redirect 'users/show_user_summary'
+    end 
   end 
 
 
