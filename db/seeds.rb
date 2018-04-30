095b1916e7b16a35f1697dd80aff9ad14b2cbbd2
Purnima Lakshmi <codequeen1110@gmail.com>
2018-04-30 17:26:21 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
 User.create(:username => "test_name", :email => "test@test.com", :password_digest => "test123")
+
+
+Dog.create(:dog_name => "dog1", :age => 1, :breed => "lab", :adoption_status => "pending")
