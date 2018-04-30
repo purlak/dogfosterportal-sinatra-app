cdcff6d844d2c32d5a508584de2d84f2ba4604f4
Purnima Lakshmi <codequeen1110@gmail.com>
2018-04-30 02:02:02 -0400

    added get route - display all dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,15 @@
 class DogsController < ApplicationController
 
+  # Display details of all dogs 
+  get '/dogs' do
+    if session[:id]
+      @dog= Dog.all 
+      erb :'dogs/dogs'
+    else 
+      #to add flash messahe
+      redirect '/login'
+    end
+  end 
   
 
 end 
