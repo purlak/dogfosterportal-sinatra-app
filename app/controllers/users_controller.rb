e0fb1011f848f6ef3e2acd03eb27694ae8282697
Purnima Lakshmi <codequeen1110@gmail.com>
2018-04-30 01:49:47 -0400

    updated login - post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,6 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-   
       if @user #&& @user.authenticate(params[:password])
         session[:id] = @user.id
         redirect '/show_user_summary'
