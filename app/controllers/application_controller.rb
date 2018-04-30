17652dba088e47aa8a7312752e417e45d87cff46
Purnima Lakshmi <codequeen1110@gmail.com>
2018-04-30 01:18:43 -0400

    updated app controller to render index page + users controller for signup route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'    
   end
 
-  get / do
+  get '/' do
     erb :index
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
 
   get '/signup' do 
     if session[:user_id]
-      redirect 'tweets/tweets'
+      redirect 'dogs/dogs'
     else 
       erb :index  
     end  
   end 
 
-  
+
 
 end 
