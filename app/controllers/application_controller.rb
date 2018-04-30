916174fb6219380182563753ea54b7b19508c694
Purnima Lakshmi <codequeen1110@gmail.com>
2018-04-30 01:06:59 -0400

    setup application controller to render index page and enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,13 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "my_application_secret"
+    set :views, Proc.new { File.join(root, "../views/") }    
   end
 
-  get "/" do
-    erb :welcome
+  get / do
+    erb :index
   end
 
 end
