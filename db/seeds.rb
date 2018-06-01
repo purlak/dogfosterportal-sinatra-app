d46b737c94f000decf1e2a365f6708a38fa65d32
Purnima Lakshmi <codequeen1110@gmail.com>
2018-05-31 23:15:39 -0400

    updated seed data field for password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create(:username => "test_name", :email => "test@test.com", :password_digest => "test123")
+User.create(:username => "test_name", :email => "test@test.com", :password => "test123")
 
 
 Dog.create(:dog_name => "dog1", :age => 1, :breed => "lab", :adoption_status => "pending")
